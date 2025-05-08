ed64fbf8eb66d595408f2b39e15d7814e33e9b63
Randú <134299499+joserandu@users.noreply.github.com>
2025-05-08 10:42:41 -0300

    Create Atv5Ex5.java

---
+++ Linguagem de Programação 1/22-04_Atv5/Atv5Ex5.java
@@ -0,0 +1,29 @@
+package atv5;
+
+import java.util.ArrayList;
+
+public class Atv5Ex5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		ArrayList<Integer> lista = new ArrayList<>();
+
+		lista.add(30);
+		lista.add(10);
+		lista.add(50);
+		lista.add(20);
+		lista.add(40);
+
+		System.out.println(lista);
+		
+		if(lista.indexOf(50) != -1) {
+			lista.remove(lista.indexOf(50));
+		}
+		
+		System.out.println(lista);
+
+	}
+
+}
